12c038f7429992d39915288dd2c1a18115c29282
sheelfshah <sheelfshah@gmail.com>
2021-05-20 01:32:50 +0530

    dead bug fix

--- backend/backendsite/backendsite/asgi.py
+++ backend/backendsite/backendsite/asgi.py
@@ -1,12 +1,12 @@
 # backendsite/asgi.py
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backendsite.settings")
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backendsite.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
